30f8d07fa8c696b507503bea6748456de51e1655
karlnh <hubigkarl@gmail.com>
2023-05-12 05:25:53 -0500

    Create main moon() function

---
+++ index.js
@@ -0,0 +1,16 @@
+const { numToArr } = require("./lib/converters");
+const { moonSum } =require("./lib/operations");
+
+function moon(op, n1, n2) {
+	let [arr1, arr2] = addZeroes(numToArr(n1), numToArr(n2));
+	switch (op) {
+	case '+':
+		return moonSum(arr1, arr2);
+	case '*':
+		return "Multiplication doesn't work yet. Sorry."
+	default:
+		return null;
+	}
+};
+
+module.exports = moon;
