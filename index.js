274c7705d56af233dd612c1a7845a88e54e07d35
karlnh <hubigkarl@gmail.com>
2023-05-14 04:10:22 -0500

    Clean up

--- index.js
+++ index.js
@@ -1,17 +1,3 @@
-// const { numToArr, addZeroes } = require("./lib/converters");
-const { moonSum } =require("./lib/operations");
-
-// function moon(op, ...args) {
-// 	switch (op) {
-// 	case '+':
-// 		return moonSum(args);
-// 	case '*':
-// 		return "Multiplication doesn't work yet. Sorry."
-// 	default:
-// 		return null;
-// 	}
-// };
-
-// console.log(moonSum(1,12,123,1234));
+const { moonSum, moonProduct } =require("./lib/operations");
 
 module.exports = moonSum;
